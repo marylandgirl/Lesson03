0935f1c6b66cea63e3175de521e414ccb6604ac1
Kim Levin <klevin5@student.umuc.edu>
2018-02-28 15:47:34 -0500

    first commit

---
+++ src/com/example/demo/HelloWorld.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorld() {
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		//get a parameter from the url
+		String message = request.getParameter("message");
+		
+		//set the parameter in the output page
+		request.setAttribute("message", message);
+		
+		//redirect to the JSP
+		request.getRequestDispatcher("/output.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
